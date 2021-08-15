c7630409cfbb132cea2d09b8bca983f2b1a293d5
jaded-x <bayne.jaded@gmail.com>
2021-08-16 00:01:55 +1000

    Time held

--- wynncraft/api.js
+++ wynncraft/api.js
@@ -19,12 +19,24 @@ async function checkTerritories() {
 			for (territory in data.territories) {
 				if (JSON.stringify(oldTList[territory].guild) !== JSON.stringify(data.territories[territory].guild)) {
 
+                    var timeAcquired = oldTList[territory].acquired.split(' ');
+                    var timeUnix = new Date(timeAcquired[0] + 'T' + timeAcquired[1] + '.000Z');
+                    var timeElapsed = data.request.timestamp * 1000 - timeUnix;
+
+                    var days = Math.floor(timeElapsed/86400000);
+                    timeElapsed -= 86400000*days;
+                    var hours = Math.floor(timeElapsed/3600000);
+                    timeElapsed -= 3600000*hours;
+                    var minutes = Math.floor(timeElapsed/60000);
+                    timeElapsed -= 60000*minutes;
+                    var seconds = Math.floor(timeElapsed/1000);
+
 					const territoryEmbed = new MessageEmbed()
 						.setColor('#0437F2')
 						.setTitle(territory)
 						.addFields(
-							{ name: data.territories[territory].guild, value: 'Has taken *' + territory + '*'},
-							{ name: oldTList[territory].guild, value: 'Has lost *' + territory + '*'}
+							{ name: data.territories[territory].guild, value: 'Has taken *' + territory + '*.'},
+							{ name: oldTList[territory].guild, value: 'Held *' + territory + '* for *' + days + '* days, *' + hours + '* hours, *'+ minutes + '* minutes and *'+ seconds + '* seconds.'}
 						)
 						.setTimestamp()
 
